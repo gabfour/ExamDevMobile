0af00163a211d1be1dba6e74ecf1e1b499639367
gabfour <gabfourtoy@gmail.com>
2023-03-28 16:42:34 +0200

    II-1 version 1 with image and link

--- trypwa/TryPWA.js
+++ trypwa/TryPWA.js
@@ -1,14 +1,19 @@
 
-export function TryPWA(params) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export function TryPWA() {
     return(
         <Div>
             <Div>
-                <Text>It seems you haven't installed our app!</Text>
-                <Text>Installing the app will provide you with a more responsive service and offline access</Text>
+                <Text>It seems you haven't installed our app yet!</Text>
+                <Text>Installing the app will provide you with a more responsive service and some offline access</Text>
                 <Text>The following browers are compatible and can let you download our app:</Text>
+                <img src={'./BrowserCompatibility.png'} />
             </Div>
             <Div>
                 <Text>If your device is compatible, you will see a download prompt next to this message</Text>
+                <FontAwesomeIcon icon="fa-regular fa-right" />
+                <Text>Alternatively, find out what feature your device has access to <a href="https://whatwebcando.today/">HERE</a></Text>
             </Div>
         </Div>
     )
